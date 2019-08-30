fcecfa164f7788574fbf190502fdde58737cefdd
malovevgen <malovevgen@yandex.ru>
2019-08-30 23:37:53 +0300

    Revert "create add_default_level_correct"
    This reverts commit 2da989bc4677ce50ece4180acdfda2a6c3895697.

--- db/migrate/20190829194831_add_default_level_correct.rb
+++
@@ -1,6 +0,0 @@
-class AddDefaultLevelCorrect < ActiveRecord::Migration[5.2]
-  def change
-    change_column_default(:tests, :level, 0)
-    change_column_default(:answers, :correct, false)
-  end
-end
