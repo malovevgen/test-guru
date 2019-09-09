a63506b73a2f9a732e1cb072000b48c16b24c79a
malovevgen <malovevgen@yandex.ru>
2019-09-10 02:28:47 +0300

    fix tests_by_level

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ApplicationRecord
   has_many :tests, through: :test_logs
   has_many :tests_created, class_name: 'Test', foreign_key: :author_id
 
-  def tests_by_level(id, level)
-    Test.joins('JOIN tests_logs ON tests.id = tests_logs.test_id')
-        .where(tests_logs: { user_id: id }, tests: { level:  level })
+  def tests_by_level(level)
+    tests.where(level: level)
   end
 end
