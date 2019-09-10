4a8305559b4781c13c5c09580acb9021e4d83010
malovevgen <malovevgen@yandex.ru>
2019-09-10 15:57:17 +0300

    classes correction

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,12 +1,12 @@
 class Test < ApplicationRecord
   belongs_to :category
-  has_many :questions
-  has_many :test_logs
-  has_many :users, through: :test_logs
+  has_many :questions, dependent: :nullify
+  has_many :test_logs, dependent: :destroy
+  has_many :users, through: :test_logs, dependent: :nullify
   belongs_to :author, class_name: 'User', foreign_key: :author_id
 
   def self.sort_categories(category)
-    joins('JOIN categories ON categories.id = tests.category_id')
+    joins(:categories)
       .where(categories: { title: category })
       .order('tests.title DESC')
       .pluck(:title)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :test_logs
-  has_many :tests, through: :test_logs
-  has_many :tests_created, class_name: 'Test', foreign_key: :author_id
+  has_many :test_logs,  dependent: :destroy
+  has_many :tests, through: :test_logs, dependent: :nullify
+  has_many :tests_created, class_name: 'Test', foreign_key: :author_id, dependent: :nullify
 
   def tests_by_level(level)
     tests.where(level: level)
