f12753a0b723287482f47ab248b2f5f374dc24fa
malovevgen <malovevgen@yandex.ru>
2019-09-12 19:10:16 +0300

    add email to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_10_173459) do
+ActiveRecord::Schema.define(version: 2019_09_12_160824) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2019_09_10_173459) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
