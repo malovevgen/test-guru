b9f54ba1acc5abff6582dfb7e74fb1127a8f2984
malovevgen <malovevgen@yandex.ru>
2019-09-13 01:41:54 +0300

    add scope

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,10 +5,14 @@ class Test < ApplicationRecord
   has_many :users, through: :logbooks, dependent: :nullify
   belongs_to :author, class_name: 'User', foreign_key: :author_id
 
-  def self.sort_categories(category)
-    joins(:categories)
+  scope :easy, -> { where(level: 0..1) }
+  scope :medium, -> { where(level: 2..4) }
+  scope :hard, -> { where(level: 5..Float::INFINITY) }
+
+  scope :sort_by_categories, ->(category) {
+    joins(:category)
       .where(categories: { title: category })
-      .order('tests.title DESC')
+      .order(title: :desc)
       .pluck(:title)
-  end
+  }
 end
