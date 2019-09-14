dcd9e9b638af98f9ecfb106ee3c8ec30ac326019
malovevgen <malovevgen@yandex.ru>
2019-09-14 14:36:19 +0300

    add validate_answers_limit

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,15 @@
 class Answer < ApplicationRecord
   belongs_to :question
 
+  validates :body, presence: true
+
+  validate :validate_answers_limit, on: :create
+
   scope :correct, -> { where(correct: true) }
 
-  validates :body, presence: true
+  private
+
+  def validate_answers_limit
+    errors.add(:base) if question.answers.count >= 4
+  end
 end
