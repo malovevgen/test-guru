e46cc26753e10be23775140dca96f6cf9a8d8b1b
malovevgen <malovevgen@yandex.ru>
2019-09-14 13:47:43 +0300

    add validations to tests

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,16 +5,18 @@ class Test < ApplicationRecord
   has_many :users, through: :logbooks, dependent: :nullify
   belongs_to :author, class_name: 'User', foreign_key: :author_id
 
+  validates :title, presence: true,
+                    uniqueness: true
+
+  validates :level, numericality: { only_integer: true, greater_than: 0 }
+
   scope :easy, -> { where(level: 0..1) }
   scope :medium, -> { where(level: 2..4) }
   scope :hard, -> { where(level: 5..Float::INFINITY) }
   scope :desc_title, -> { order(title: :desc).pluck(:title) }
-
   scope :sort_by_categories, ->(category) {
     joins(:category)
       .where(categories: { title: category })
       .desc_title
   }
-
-  validates :title, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_12_185900) do
+ActiveRecord::Schema.define(version: 2019_09_14_102135) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2019_09_12_185900) do
     t.integer "author_id"
     t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
   end
 
   create_table "users", force: :cascade do |t|
@@ -61,5 +62,4 @@ ActiveRecord::Schema.define(version: 2019_09_12_185900) do
     t.datetime "updated_at", null: false
     t.string "email"
   end
-
 end
