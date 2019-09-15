5efaf684bd43da4bbc1cf34a9eb3647634fb562a
malovevgen <malovevgen@yandex.ru>
2019-09-15 22:02:23 +0300

    to previos commit

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,12 +1,12 @@
 class Test < ApplicationRecord
   belongs_to :category
-  belongs_to :author, class_name: 'User', foreign_key: :author_id  
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_many :questions, dependent: :nullify
   has_many :logbooks, dependent: :destroy
-  has_many :users, through: :logbooks, dependent: :nullify
+  has_many :users, through: :logbooks
 
   validates :title, presence: true
-  
+
   validates :level, numericality: { only_integer: true, greater_than: 0 }
 
   scope :easy, -> { where(level: 0..1) }
