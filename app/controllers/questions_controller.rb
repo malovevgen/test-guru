5deb345632b7be5ab858b12e60b2cf2e1722654b
malovevgen <malovevgen@yandex.ru>
2019-09-18 01:38:58 +0300

    add #self.before_action to controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,21 @@
 class QuestionsController < ApplicationController
+  before_action :find_test, only: %i[index create new]
+  before_action :find_question, only: %i[show destroy]
+
   def index
-    render inline: 'List of test questions: <%= Test.find(params[:test_id]).questions.pluck(:body) %>'
+    render inline: 'List of test questions: <%= @test.questions.pluck(:body) %>'
+  end
+
+  def new
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
   end
 end
