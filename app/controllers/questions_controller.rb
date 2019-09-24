88556b47a771ebd608ac664cf7c5f36b6d7ca353
malovevgen <malovevgen@yandex.ru>
2019-09-24 23:45:00 +0300

    add _question and fix questions_controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -14,13 +14,29 @@ class QuestionsController < ApplicationController
 
   def new; end
 
+  def edit; end
+
   def destroy
     @question.destroy
+    redirect_to @test
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to @test
+    else
+      render :edit
+    end
   end
 
   def create
-    question = @test.questions.create(question_params)
-    redirect_to question
+    @question = @test.questions.new(question_params)
+
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end
   end
 
   private
