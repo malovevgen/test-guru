4692c8493aecf72a92e40e0b83f13b3644e15972
malovevgen <malovevgen@yandex.ru>
2019-09-28 21:01:34 +0300

    rails g scaffold answer --no-migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   #root to: 'questions#index' #set the Root route
   resources :tests do
-    resources :questions, except: :index, shallow: true
+    resources :questions, shallow: true, except: :index do
+      resources :answers, shallow: true, except: :index
+    end
   end
-end 
+end
