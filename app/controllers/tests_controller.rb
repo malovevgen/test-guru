f2130758c9f06560143edb070f1a561fd72e5b59
malovevgen <malovevgen@yandex.ru>
2019-09-29 18:27:03 +0300

    modifications tests and test_passages controllers views and routes

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,5 +1,6 @@
 class TestsController < ApplicationController
-  before_action :find_test, only: %i[show destroy edit update]
+  before_action :find_test, only: %i[show destroy edit update start]
+  before_action :set_user, only: :start
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
 
@@ -40,6 +41,11 @@ class TestsController < ApplicationController
     end
   end
 
+  def start
+    @user.tests << @test #takes only one argument and puts it at the end of the array
+    redirect_to @user.test_passage(@test)
+  end
+
   private
 
   def find_test
@@ -48,6 +54,10 @@ class TestsController < ApplicationController
 
   def test_params
     params.require(:test).permit(:title, :level, :category_id, :author_id)
+  end
+
+  def set_user
+    @user = User.first
   end
 
   def rescue_with_test_not_found
