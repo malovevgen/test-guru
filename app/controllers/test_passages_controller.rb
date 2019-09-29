84c7537f56bf5ff1fcf28ad67bbc69ce55223f72
malovevgen <malovevgen@yandex.ru>
2019-09-30 00:39:24 +0300

    add result

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -2,17 +2,18 @@ class TestPassagesController < ApplicationController
 
   before_action :set_test_passage, only: %i[show update result]
 
-  def show
+  def show; end
 
-  end
-
-  def result
-
-  end
+  def result; end
 
   def update
     @test_passage.accept!(params[:answer_ids])
-    render :show
+
+    if @test_passage.completed?
+      redirect_to result_test_passage_path(@test_passage)
+    else
+      render :show
+    end
   end
 
   private
--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -5,26 +5,25 @@ class TestPassage < ApplicationRecord
 
   before_validation :before_validation_set_first_question, on: :create
 
-  def accept!(answer_ids)
-    if correct_answer?(answer_ids)
-      self.correct_questions += 1
-    end
+  def completed?
+    current_question.nil?
+  end
 
+  def accept!(answer_ids)
+
+    self.correct_questions += 1 if correct_answer?(answer_ids)
     self.current_question = next_question
-    save
+    save!
   end
 
   private
 
   def before_validation_set_first_question
-    self.current_question = test.questions.first if test.present?
+    self.current_question = next_question
   end
 
-  def correct_answer(answer_ids)
-    correct_answers_count = correct_answers.count
-
-    (correct_answers_count == correct_answers.where(id: answer_ids).count) &&
-    correct_answers_count == answer_ids.count
+  def correct_answer?(answer_ids)
+    correct_answers.ids.sort == Array(answer_ids).map(&:to_i).sort
   end
 
   def correct_answers
@@ -32,6 +31,10 @@ class TestPassage < ApplicationRecord
   end
 
   def next_question
-    test.questions.order(:id).where('id > ?', current_question.id).first
+    if new_record?
+      test.questions.first
+    else
+      test.questions.order(:id).where('id > ?', current_question.id).first
+    end
   end
 end
