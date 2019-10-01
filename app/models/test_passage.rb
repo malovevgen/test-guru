7f43eb0c4b895369587bc27cd3f69ee009aec864
malovevgen <malovevgen@yandex.ru>
2019-10-01 22:00:40 +0300

    question number and total questions

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -3,14 +3,21 @@ class TestPassage < ApplicationRecord
   belongs_to :user
   belongs_to :current_question, class_name: 'Question', optional: true
 
-  before_validation :before_validation_set_first_question, on: :create
+  before_validation :before_validation_set_question, on: :create
 
   def completed?
     current_question.nil?
   end
 
-  def accept!(answer_ids)
+  def total_questions
+    test.questions.count
+  end
 
+  def current_question_number
+     test.questions.order(:id).where('id < ?', current_question.id).count + 1
+  end
+
+  def accept!(answer_ids)
     self.correct_questions += 1 if correct_answer?(answer_ids)
     self.current_question = next_question
     save!
@@ -18,7 +25,7 @@ class TestPassage < ApplicationRecord
 
   private
 
-  def before_validation_set_first_question
+  def before_validation_set_question
     self.current_question = next_question
   end
 
