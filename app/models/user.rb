e0df2b9ce5a4b64b4fc78c82c855e093265ee873
malovevgen <malovevgen@yandex.ru>
2019-10-04 22:01:43 +0300

    add module auth.rb and bundle instal

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,13 @@
 
 class User < ApplicationRecord
 
-  attr_reader :password
-  attr_writer :password_confirmation
+  include Auth
 
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
   has_many :tests_created, class_name: 'Test', foreign_key: :author_id, dependent: :nullify
 
-  validates :name, :email, presence: true
-  validates :password, presence: true, if: Proc.new { |u| u.password_digest.blank? }
-  validates :password, confirmation: true
+  has_secure_password
 
   def test_passage(test)
     test_passages.order(updated_at: :desc).find_by(test_id: test.id) # order(:id)
@@ -19,24 +16,5 @@ class User < ApplicationRecord
 
   def tests_by_level(level)
     tests.where(level: level)
-  end
-
-  def authenticate(password_string)
-    digest(password_string) == self.password_digest ? self : false
-  end
-
-  def password=(password_string)
-    if password_string.nil?
-      self.password_digest = nil
-    elsif password_string.present?
-      @password = password_string
-      self.password_digest = digest(password_string)
-    end
-  end
-
-  private
-
-  def digest(string)
-    Digest::SHA1.hexdigest(string)
   end
 end
