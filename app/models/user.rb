255201ba6c6bf9046cad38cd01a3fb65e875dc6a
malovevgen <malovevgen@yandex.ru>
2019-10-05 01:10:32 +0300

    sessions controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,16 @@
 
 class User < ApplicationRecord
 
-  include Auth
+  attr_reader :password
+  attr_writer :password_confirmation
 
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
   has_many :tests_created, class_name: 'Test', foreign_key: :author_id, dependent: :nullify
+
+  validates :name, :email, presence: true
+  validates :password, presence: true, if: Proc.new { |u| u.password_digest.blank? }
+  validates :password, confirmation: true
 
   has_secure_password
 
