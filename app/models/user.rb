e49a9c2b2d2f5b1b9ce47237c77e8ed1d0b79d10
malovevgen <malovevgen@yandex.ru>
2019-10-06 21:38:47 +0300

    rails g devise User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 #require 'digest/shal'
 
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
   attr_reader :password
   attr_writer :password_confirmation
