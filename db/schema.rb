e0e0e92e0c6ca0dcbea998fa7fa6638b27030118
malovevgen <malovevgen@yandex.ru>
2019-10-06 23:27:58 +0300

    add_index_to_users_email.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_06_183344) do
+ActiveRecord::Schema.define(version: 2019_10_06_201506) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 2019_10_06_183344) do
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
