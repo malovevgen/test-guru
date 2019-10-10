98bcbd4352fede71122a9262d010b93290beda86
malovevgen <malovevgen@yandex.ru>
2019-10-10 22:53:18 +0300

    Confirm my account

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
 
-  devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout}
-  # get 'sessions/new'
-  # get 'users/new'
+  root 'tests#index'
 
-  # delete :logout, to: 'sessions#destroy'
+  devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout}
 
   resources :tests do
     resources :questions, shallow: true, except: :index do
@@ -14,7 +12,6 @@ Rails.application.routes.draw do
     post :start, on: :member
   end
 
-  #  GET /test_passages/101/result
   resources :test_passages, only: %i[show update] do
 
     get :result, on: :member
