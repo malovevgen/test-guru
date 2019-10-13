c7a27fa4dc2d1aa37c84113d7a85ee5e21aebe5c
malovevgen <malovevgen@yandex.ru>
2019-10-13 21:47:00 +0300

    localization in Rails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
+
+  before_action :set_locale
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def default_url_options
+    { lang: I18n.locale }
+  end
 
   protected
 
@@ -15,6 +21,10 @@ class ApplicationController < ActionController::Base
                                                        :last_name,
                                                        :password,
                                                        :password_confirmation])
+  end
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 
 end
