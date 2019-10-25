70b8954a06d18b6a0f62cdc25736c33f7193d8ee
malovevgen <malovevgen@yandex.ru>
2019-10-25 23:44:31 +0300

    link to the created gist

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -21,13 +21,16 @@ class TestPassagesController < ApplicationController
   def gist
     result = GistQuestionService.new(@test_passage.current_question).call
 
-    flash_options = if result.html_url.present?
-                       { notice: t('.success') }
-                    else
-                       { alert: t('.failure') }
-    end
+      if result.html_url.present?
+                      Gist.create!(question: @test_passage.current_question, user: @test_passage.user, gist_url: result.html_url)
+      flash[:notice] = "#{t('.success') } #{view_context.link_to(t('.view_gist'),
+                                                                         result.html_url,
+                                                                         target: "_blank")}"
+      else
+      flash[:alert] = t('.failure')
+      end
 
-    redirect_to @test_passage, flash_options
+    redirect_to @test_passage
   end
 
   private
