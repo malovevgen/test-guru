fa606352b0585c4ae0db8c4dd9e95f70656a0e9b
malovevgen <malovevgen@yandex.ru>
2019-11-10 16:08:51 +0300

    create form_inline

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -1,6 +1,7 @@
 class Admin::TestsController < Admin::BaseController
 
-  before_action :find_test, only: %i[show destroy edit update start]
+  before_action :find_tests, only: %i[index update_inline]
+  before_action :find_test, only: %i[show destroy edit update start update_inline]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
 
@@ -14,6 +15,14 @@ class Admin::TestsController < Admin::BaseController
 
   def new
     @test = Test.new
+  end
+
+  def update_inline
+    if @test.update(test_params)
+      redirect_to admin_tests_path
+    else
+      render :index
+    end
   end
 
   def destroy
@@ -42,6 +51,10 @@ class Admin::TestsController < Admin::BaseController
   end
 
   private
+
+  def find_tests
+    @tests = Test.all
+  end
 
   def find_test
     @test = Test.find(params[:id])
