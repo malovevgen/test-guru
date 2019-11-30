fa6903716ff4500dcac6c9bf7a03fe8a6cf54cff
malovevgen <malovevgen@yandex.ru>
2019-11-30 23:24:02 +0300

    Added email feld

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -8,15 +8,19 @@ class FeedbacksController < ApplicationController
 
     if @feedback.valid?
       FeedbacksMailer.send_feedback(@feedback).deliver_now
+      flash.notice = t('.success')
       redirect_to root_path
     else
+      flash.alert = t('.failure')
+
       render :new
     end
+    #redirect_to root_path
   end
 
   private
 
   def feedback_params
-    params.require(:feedback).permit(:message)
+    params.require(:feedback).permit(:email, :message)
   end
 end
--- app/mailers/feedbacks_mailer.rb
+++ app/mailers/feedbacks_mailer.rb
@@ -2,6 +2,7 @@ class FeedbacksMailer  <  ApplicationMailer
 
   def send_feedback(feedback)
     @message = feedback.message
+    @email = feedback.email
     mail to: 'evgenymalov76@gmail.com'
   end
 end
