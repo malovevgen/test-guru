a4eccc62116c60442b4d307178596db28a161a73
malovevgen <malovevgen@yandex.ru>
2020-01-07 16:46:18 +0300

    badges/index

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -7,6 +7,6 @@ class BadgesController < ApplicationController
     # render html: '<h1>All badges</h1>'.html_safe
     # render json: { badges: Badge.all }
     # render inline: '<p>My favorite languge is: <%= %[ybuR].reverse! %> </p>'
-    render file: 'badges/all_level', layout: false
+    # render file: 'badges/all_level', layout: false
   end
 end
