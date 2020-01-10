454b17f4c04092477a907557bddfe76de4ff8235
malovevgen <malovevgen@yandex.ru>
2020-01-10 22:35:42 +0300

    create views/admin/badges/edit and add description to badges

--- app/controllers/admin/badges_controller.rb
+++ app/controllers/admin/badges_controller.rb
@@ -1,11 +1,26 @@
 class Admin::BadgesController < Admin::BaseController
-  def edit
-  end
 
-  def show
-  end
+  before_action :find_badge, only: %i[show edit update]
+
+  def edit; end
+
+  def show; end
 
   def update
+    if @badge.update(badge_params)
+      redirect_to [:admin, @badge]
+    else
+      render :edit
+    end
   end
-  
+
+  private
+
+  def find_badge
+    @badge = Badge.find(params[:id])
+  end
+
+  def badge_params
+    params.require(:badge).permit(:status)
+  end
 end
