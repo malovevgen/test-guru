7ec10b5331092fc3ff1a480d11cba5df4d3f29db
malovevgen <malovevgen@yandex.ru>
2020-02-16 00:54:48 +0300

    corrections in views

--- app/helpers/admin/badges_helper.rb
+++ app/helpers/admin/badges_helper.rb
@@ -1,13 +1,5 @@
 module Admin::BadgesHelper
   def rules
-    Badge::RULES.map { |rule | [t("badges.rules#{rule}"), rule]}
-  end
-
-  def badge_rules
-    [
-      [t("helpers.select.rules.category"), :category],
-      [t("helpers.select.rules.level"), :level],
-      [t("helpers.select.rules.first_attempt"), :first_attempt]
-    ]
+    Badge::RULES.map { |rule | [t("badges.rules#{rule}"), rule] }
   end
 end
